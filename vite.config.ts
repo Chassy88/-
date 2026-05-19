7633b39f306d3f8952c65cdbac56420bff7d2018
Chassy88 <seitaro0813@gmail.com>
2026-05-20 07:02:37 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cheer-lab/', // ★GitHub Pagesで公開するために、この一行が絶対に必要です！
+  base: '/cheer-lab/', 
 })
